56e7c9ff9189f221b5f5075184b5e2c10f26f0b1
Juan David Davalos <juandm417@gmail.com>
2024-05-23 18:43:48 -0500

    Se crea la pagina de inventarios con su Crud y se añade al layout

---
+++ Pages/Itinerarios/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AirBook.Models;
+using AirBook.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirBook.Pages.Itinerarios
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AirBookDbContext _context;
+
+        public IndexModel(AirBookDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Itinerario> Itinerarios { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.Itinerarios != null)
+            {
+                Itinerarios = await _context.Itinerarios
+                    .Include(r => r.Reserva)
+                    .ToListAsync();
+            }
+        }
+    }
+}
